7757776d3cf6e7856d54b84f88d48ae5c73c7ee0
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-06 01:34:37 +0530

    add: application helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  def current_user
-    if session[:user_id]  
-      @current_user ||= User.find_by(id: session[:user_id])
+  before_action :logged_out_user
+
+  def logged_in_user
+    if logged_in?
+      redirect_to dashboard_path
+    end
+  end
+
+  def logged_out_user
+    unless logged_in?
+      respond_to do |format|
+        format.html do
+          flash[:danger] = "Please Log In"
+          redirect_to login_path
+        end
+        format.json { render status: :unauthorized, json: { errors: "You need to login" } }
+      end
     end
   end
 end
