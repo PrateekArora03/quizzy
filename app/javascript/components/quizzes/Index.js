015774d8c255a5a92178e2130f3848a4576f94bd
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-11 01:34:06 +0530

    add: Quiz table component and fix Quiz form

--- app/javascript/components/quizzes/Index.js
+++ app/javascript/components/quizzes/Index.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import QuizTable from "./QuizTable";
+import Alert from "../layouts/Alert";
 
-const Index = () => {
+const Index = ({ quizzes }) => {
+  const [messages, setMessages] = useState({});
+
   return (
-    <div className="d-flex justify-content-end">
-      <a href="/quizzes/new" className="btn btn-primary">
-        Add new quiz
-      </a>
-    </div>
+    <Fragment>
+      {Object.keys(messages).length > 0 && <Alert messages={messages} />}
+      <div className="d-flex justify-content-end">
+        <a href="/quizzes/new" className="btn btn-primary">
+          Add new quiz
+        </a>
+      </div>
+      <div className="d-flex mt-3 justify-content-center">
+        {quizzes.length > 0 ? (
+          <QuizTable quizzes={quizzes} setMessages={setMessages} />
+        ) : (
+          <span className="text-muted">You have not created any quiz.</span>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
---
+++ app/javascript/components/quizzes/QuizTable.js
@@ -0,0 +1,91 @@
+import React, { Fragment } from "react";
+import { useTable } from "react-table";
+import API from "../../utils/API";
+
+function Table({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <table className="table table-striped" {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function QuizTable({ quizzes, setMessages }) {
+  const deleteQuiz = async (id) => {
+    try {
+      const quizDelete = confirm("Are you sure you want to delete this Quiz?");
+      if (quizDelete) {
+        await API(`/quizzes/${id}`, "delete");
+        window.location.href = "/";
+      }
+    } catch ({ response }) {
+      response.data.type = "danger";
+      setMessages(response.data);
+    }
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Quiz Name",
+        accessor: "name",
+      },
+      {
+        Header: "Edit",
+        accessor: "id",
+        Cell: ({ value }) => (
+          <a href={`/quizzes/${value}/edit`} className="btn btn-warning">
+            Edit
+          </a>
+        ),
+      },
+      {
+        Header: "Delete",
+        Cell: (props) => (
+          <button
+            className="btn btn-danger"
+            onClick={() => deleteQuiz(props.data[props.row.index].id)}
+          >
+            Delete
+          </button>
+        ),
+      },
+    ],
+    []
+  );
+
+  return <Table columns={columns} data={quizzes} />;
+}
+
+export default QuizTable;
