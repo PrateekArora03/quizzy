455f0881987903236ea76d55c39ade7aee03084d
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-11 01:32:05 +0530

    fix: add edit, update, delete routes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,8 @@
 class QuizzesController < ApplicationController
+  before_action :load_quiz, only: [:edit, :update, :destroy]
 
   def index
-    render
+    @quizzes = current_user.quizzes.as_json(only: [:id, :name])
   end
 
   def new
@@ -18,8 +19,41 @@ class QuizzesController < ApplicationController
     end
   end
 
+  def edit
+    render
+  end
+
+  def update
+    if @quiz.update(quiz_params)
+      flash[:success] = "Quiz updated!"
+    else
+      render status: :unprocessable_entity, json: { errors: @quiz.errors.full_messages }
+    end
+  end
+
+  def destroy
+    if @quiz.destroy
+      flash[:success] = "Quiz deleted!"
+    else
+      render status: :unprocessable_entity, json: { errors: @quiz.errors.full_messages }
+    end
+  end
+
   private
     def quiz_params
       params.require(:quiz).permit(:name)
+    end
+
+    def load_quiz
+    @quiz = current_user.quizzes.find(params[:id])
+      rescue ActiveRecord::RecordNotFound => errors
+        respond_to do |format|
+          format.html do
+            flash[:warning] = "Quiz not found!"
+            redirect_to quizzes_path
+          end
+          format.json { render status: :not_found, json: {errors: ["Quiz not found!"] }
+        }
+        end
     end
 end
