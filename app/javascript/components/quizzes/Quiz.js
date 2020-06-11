7edb99195aa93ea410a63d5c649594b7547f16af
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-11 05:37:03 +0530

    add: show components and update route for show action

---
+++ app/javascript/components/quizzes/Quiz.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+
+const Quiz = ({ quiz }) => {
+  return (
+    <Fragment>
+      <div className="d-flex justify-content-end">
+        <a href="#" className="btn btn-primary">
+          Add questions
+        </a>
+      </div>
+      <div className="d-flex mt-3 justify-content-center">
+        <span className="text-muted">There are no questions in this quiz.</span>
+      </div>
+    </Fragment>
+  );
+};
+
+Quiz.prototype = {
+  quiz: PropTypes.object.isRequired,
+};
+
+export default Quiz;
