8335781e1ac28c6aaf1860ce84fbddae68021204
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-11 12:24:27 +0530

    fix: remove: only from quizzes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "quizzes#index"
-  resources :quizzes, only: [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :quizzes
   resource :sessions, only: [:new, :create, :destroy]
 end
