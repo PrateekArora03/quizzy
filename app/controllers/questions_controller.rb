76a9dad15d275d0feadf2c988abafe73597cc476
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-12 23:00:34 +0530

    add: QuestionForm react component

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,7 @@
 class QuestionsController < ApplicationController
+
+  def new
+    @quiz = current_user.quizzes.find(params[:quiz_id]);
+    render
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "quizzes#index"
-  resources :quizzes
+  resources :quizzes do
+    resources :questions, only: [:new, :create]
+  end
   resource :sessions, only: [:new, :create, :destroy]
 end
