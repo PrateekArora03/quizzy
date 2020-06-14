d1b13b35a0858e47955e97eb9a8fa4edce22db40
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-14 23:19:15 +0530

    add: edit, update and destroy action into question

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
     end
 
     def load_quiz
-      @quiz = current_user.quizzes.find(params[:id] || params[:quiz_id]);
+      @quiz = current_user.quizzes.find(params[:quiz_id] || params[:id]);
       rescue ActiveRecord::RecordNotFound => errors
         respond_to do |format|
           format.html do
@@ -30,6 +30,19 @@ class ApplicationController < ActionController::Base
             redirect_to quizzes_path
           end
           format.json { render status: :not_found, json: {errors: ["Quiz not found!"] }
+        }
+      end
+    end
+
+    def load_question
+      @question = @quiz.questions.find(params[:id]);
+      rescue ActiveRecord::RecordNotFound => errors
+        respond_to do |format|
+          format.html do
+            flash[:warning] = "Question not found!"
+            redirect_to quizzes_path
+          end
+          format.json { render status: :not_found, json: {errors: ["Question not found!"] }
         }
       end
     end
