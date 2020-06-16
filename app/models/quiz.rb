68ded139c847a5c18f4222c59a11ac1995600a3e
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-16 22:22:49 +0530

    fix: add slug into quiz table

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,15 @@ class Quiz < ApplicationRecord
   belongs_to :user
   has_many :questions, dependent: :destroy
   validates :name, presence: true, length: { minimum: 4 }
+
+  private
+    def generate_slug
+      parameterize_slug = self.name.parameterize
+      index = 0
+      loop do
+        break unless Quiz.exists?( slug: parameterize_slug ) if index == 0
+        break unless Quiz.exists?( slug: parameterize_slug+"-"+index )
+        index++
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_214758) do
+ActiveRecord::Schema.define(version: 2020_06_15_183729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_06_11_214758) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_quizzes_on_slug", unique: true
     t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
