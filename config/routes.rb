4cc5c481c701ed6bbc82a6a2f1068d2208d595a1
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-17 00:28:53 +0530

    add: attempt controller into quizzy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "quizzes#index"
+  resources :public, only: [] do
+    resources :attempts, only: [:new]
+  end
   resources :quizzes do
     resources :questions, except: [:index, :show]
   end
