72798e228164f127fb31eff70d596875f771dcaf
Prateek Arora <prateekisthebest98@gmail.com>
2020-06-28 01:14:50 +0530

    add: service for download report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,23 @@
 class ReportsController < ApplicationController
 
+  def create
+    job_id = CreateReportWorker.perform_async(current_user.id)
+    render status: :ok, json: { processing: true, notice: "Processing", job_id: job_id }
+  end
+
   def show
     @quizzes = current_user.quizzes.published
+    respond_to do |format|
+      format.html
+      format.csv do
+        if params[:job_id]
+          p current_job = current_user.jobs.find_by(job_id: params[:job_id])
+          if current_job && current_job[:status] == "done"
+            return send_file current_job.filename, type: "application/csv"
+          end
+          render status: :ok, json: { processing: true, notice: "Processing", job_id: current_job }
+        end
+      end
+    end
   end
 end
