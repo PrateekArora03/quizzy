8f402d617eb998d568bad34eeb783f3e888347f1
Prateek Arora <prateekisthebest98@gmail.com>
2020-07-03 00:27:58 +0530

    fix: improve formating

--- challenge-price-calculator/price_calculator.rb
+++ challenge-price-calculator/price_calculator.rb
@@ -1,15 +1,21 @@
 item_unit_price = { Milk: 3.97, Bread: 2.17, Banana: 0.99, Apple: 0.89}
 sale_on_items = { Milk: { price: 5.00, quantity: 2 }, Bread: { price: 6.00, quantity: 3 } }
+
 puts "Please enter all the items purchased separated by a comma:"
 items_list = gets.chomp
 puts "Item \t Quantity \t Price \n" + "-"*30
+
 cart = { products: {}, saved: 0.00, total_price: 0.00 }
+
 items_list.split(",").each do |item|
   item = item.strip.capitalize().to_sym
+
   if cart[:products][item].nil?
     cart[:products][item] = { quantity: 0, price: 0.00 }
   end
+
   cart[:products][item][:quantity] += 1
+  
   if sale_on_items.key?(item) && cart[:products][item][:quantity] % sale_on_items[item][:quantity] == 0 && cart[:products][item][:quantity] != 0
     cart[:products][item][:price] -= (sale_on_items[item][:quantity] - 1) * item_unit_price[item]
     cart[:products][item][:price] += sale_on_items[item][:price]
